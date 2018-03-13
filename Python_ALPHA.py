51b16b2d79008565ddf3de84c3fac4a492f23e61
Salabh SG <salabhsg@gmail.com>
2018-03-13 22:17:30 +0530

    Added Excercise no.2
    Added Excercise no.2. Multipying and concatenating lists

--- Python_ALPHA.py
+++ Python_ALPHA.py
@@ -1,3 +1,5 @@
+#Excercise (1)=====================================>>>>>>>>>>>
+
 numbers = []
 strings = []
 names = ["John", "Eric", "Jessica"]
@@ -12,3 +14,23 @@ second_name = names[1]
 print(numbers)
 print(strings)
 print("The second name on the names list is %s" % second_name)
+
+#Excercise (2)=======================================>>>>>>>>>>>
+
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
